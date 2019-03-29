3933efaccc18c2bfcca4dbf30d4afa997307b2f9
rahus12 <47785064+rahus12@users.noreply.github.com>
2019-03-29 19:59:58 +0530

    Add files via upload

---
+++ code1.m
@@ -0,0 +1,42 @@
+clc;
+all_box=0;
+ButtonHandle = uicontrol('Style', 'PushButton', ...
+                         'String', 'Stop loop', ...
+                         'Callback', 'delete(gcbf)');
+                     
+cam=webcam;
+for l = 1:1e6
+    img=snapshot(cam);
+    %subplot(2,2,1);
+    imshow(img);
+    bi=im2bw(img,0.99);
+    bi=bwareaopen(bi,20);
+    bi=imfill(bi,'holes');
+    
+    %%binary iage
+    %subplot(2,2,2)
+    %imshow(bi);
+    
+  %%bounding box
+  measurements = regionprops(bi, 'BoundingBox', 'Area');
+  allAreas = [measurements.Area];
+  [mValue, mIndex]=max(allAreas);
+  k=mIndex;
+  if(k>0)
+
+  thisBB = measurements(k).BoundingBox;
+    all_box(k) = thisBB(3)*thisBB(4);
+    rectangle('Position', [thisBB(1),thisBB(2),thisBB(3),thisBB(4)],...
+    'EdgeColor','b','LineWidth',2 )
+
+    xval=thisBB(1)+(thisBB(3)/2)
+  end
+ 
+%%button to stop
+  if ~ishandle(ButtonHandle)
+    disp('Loop stopped by user');
+    break;
+  end
+  pause(0.02)
+end 
+clear('cam');
